b65dd052ccb9f0921f282a7729c9d398a50d98df
Anastasios Klopas <klopas2020@gmail.com>
2021-05-07 17:35:50 +0300

    added new welcome message

--- TicTacToe_V1/Program.cs
+++ TicTacToe_V1/Program.cs
@@ -10,6 +10,7 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
             MainGame();
         }
         private static void MainGame()
